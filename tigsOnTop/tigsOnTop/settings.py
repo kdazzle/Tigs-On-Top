f0ee022fca32ad0240c1246919b82478e34a7279
Kyle Valade <kylevalade@gmail.com>
2014-06-29 18:31:02 -0700

    Add logging

--- tigsOnTop/tigsOnTop/settings.py
+++ tigsOnTop/tigsOnTop/settings.py
@@ -162,13 +162,34 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.expanduser('~/logs/user/info_tigsontop.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5MB
+            'backupCount': 5
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler'
         }
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['mail_admins', 'file', 'console'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'django': {
+            'handlers': ['file', 'console'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        '': {
+            'handlers': ['file', 'console'],
+            'level': 'INFO',
+            'propagate': True
         },
     }
 }
